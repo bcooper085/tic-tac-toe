4d6686309825dae389d61e9df204556a6552bcb7
Brandon Cooper and Marc Larkin <brandon@>
2017-01-26 15:31:35 -0800

    add method to hide clicked divs by manipulating their z-index in css

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 ## Specs
 
-* Create a 3x3 game board --- object
-* Create pieces to use in the game --- images
-* Create ability to place pieces --- click function
+* Create a 3x3 game board --- object √
+* Create pieces to use in the game --- images √
+* Create ability to place pieces --- click function √
+* Remove ability to add/remove pieces after they have been placed
 * Create players --- object
 * make active and inactive states for players key: value in player object
 * Create separate pieces for each player --- more images, classed to each player
--- js/scripts.js
+++ js/scripts.js
@@ -2,26 +2,6 @@
 var activePlayer1 = 1;
 var activePlayer2 = 0;
 
-function Player(playerName, playerNumber, playerSymbol) {
-  this.playerName = playerName;
-  this.playerNumber = playerNumber;
-  this.playerSymbol = playerSymbol;
-}
-var player1 = new Player("Brandon", 1, "X");
-var player2 = new Player("Marc", 2, "O");
-
-var position = [0,0,0,0,0,0,0,0,0]
-
-if (Player.playerNumber === 1) {
-  Player.playerSymbol = "X";
-} else {
-  Player.playerSymbol = "O";
-}
-
-// function Position(position) {
-//   this.position = position;
-// }
-
 function switchPlayer() {
   if (activePlayer1 === 1) {
     activePlayer1 = 0;
@@ -31,25 +11,16 @@ function switchPlayer() {
   }
 };
 
-var one = position[0];
-var two = position[1];
-var three = position[2];
-var four = position[3];
-var five = position[4];
-var six = position[5];
-var seven = position[6];
-var eight = position[7];
-var nine = position[8];
-
-
-
 //user logic
 $(function() {
   $(".clickbox").click(function() {
     if (activePlayer1 === 1) {
+      $(this).addClass("filled");
       $(this).children("span").text("X");
+
     } else {
-      $(this).children("span").text("O");
+      $(this).addClass("filled");
+      $(this).children("span").text("O")
     }
     switchPlayer();
   })
